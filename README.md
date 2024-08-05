0a3cf0644b3e18f74af8c020586e78675b83c799
Aadi-kommula <aadikommula@gmail.com>
2024-08-05 23:00:33 +0530

    uploading all assests

--- README.md
+++
@@ -1,3 +0,0 @@
-Weather-app using react 
-We can get the Weather details from this application by simply providing the major city names 
-check it out at https://aadi-webapps.free.nf/wether-app/
---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './Header.css'
+function
+Header() {
+    return (
+    
+    <div className="header">   
+    <h3></h3>
+            
+        <nav>            
+        <a href="http://aadi-kommula.unaux.com/" target="_blank">HOME</a>
+        <a href="#">CONTACT US </a>
+        <a href="#">SERVICES</a>
+        <a href="#">HELP</a>
+
+        </nav>
+        
+        
+    </div>)
+}
+export default Header;
